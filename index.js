6f5b0fc26a141c5693f474bcd255fa8defae0ed0
Shiv Arora <shiv.arora@deeplogictech.com>
2025-02-11 17:51:01 +0530

    Added the session

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const session = require("express-session")
 
 const app = express();
 
 app.set('view engine','ejs');
 
+app.use(express.urlencoded({extended:false}));
+
+app.use(session({
+    secret: 'my_session_secret',
+    resave: true,
+    saveUninitialized : false
+}))
+
 app.get('/',(req,res)=>{
     res.render('home');
-})
+});
+
+app.get('/login',(req,res)=>{
+    res.render('login',{error:null});
+});
+
+app.post('/login',(req,res)=>{
+    if(req.body.username==='john' && req.body.password==='123'){
+        req.session.user = { id:1,username:'john',name:'John Doe' };
+        res.redirect('/');
+    } else {
+        res.render('login',{ error:"Wrong credentials"});
+    }
+});
 
 app.listen(3000,()=> console.log("server started at port 3000"));
